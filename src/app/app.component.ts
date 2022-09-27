2c3a560a72979d0d2e63e40c60581ed2ff0b9609
Min Thway Khaing <minthwaykhaing28@gmail.com>
2022-09-27 10:22:36 +0630

    inited

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+@Component({
+  animations: [
+    trigger('filterAnimation', [
+      transition(':enter, * => 0, * => -1', []),
+      transition(':increment', [
+        query(':enter', [
+          style({ opacity: 0, width: 0 }),
+          stagger(50, [
+            animate('300ms ease-out', style({ opacity: 1, width: '*' })),
+          ]),
+        ], { optional: true })
+      ]),
+      transition(':decrement', [
+        query(':leave', [
+          stagger(50, [
+            animate('300ms ease-out', style({ opacity: 0, width: 0 })),
+          ]),
+        ])
+      ]),
+    ]),
+  ]
+})
+export class HeroListPageComponent implements OnInit {
+  heroesTotal = -1;
+
+  get heroes() { return this._heroes; }
+  private _heroes: Hero[] = [];
+
+  ngOnInit() {
+    this._heroes = HEROES;
+  }
+
+  updateCriteria(criteria: string) {
+    criteria = criteria ? criteria.trim() : '';
+
+    this._heroes = HEROES.filter(hero => hero.name.toLowerCase().includes(criteria.toLowerCase()));
+    const newTotal = this.heroes.length;
+
+    if (this.heroesTotal !== newTotal) {
+      this.heroesTotal = newTotal;
+    } else if (!criteria) {
+      this.heroesTotal = -1;
+    }
+  }
+}
